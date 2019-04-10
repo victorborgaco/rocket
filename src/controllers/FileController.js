ffa2a86ee221b226c3473bea331d3f2b84de72b7
Victor Borgaco <victor.borgaco@gmail.com>
2019-04-09 23:36:05 -0300

    correção url/porta

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -4,10 +4,12 @@ const Box = require('../models/Box')
 class FileController {
   async store (req, res) {
     const box = await Box.findById(req.params.id)
+
     const file = await File.create({
       title: req.file.originalname,
       path: req.file.key
     })
+
     box.files.push(file)
 
     await box.save()
--- src/models/File.js
+++ src/models/File.js
@@ -16,7 +16,7 @@ const File = new mongoose.Schema(
     toJSON: {virtuals: true}
   })
 File.virtual('url').get(function () {
-  const url = process.env.PORT || 'http://localhost:3333'
+  const url = process.env.URL || 'http://localhost:3333'
   return `${url}/files/${encodeURIComponent(this.path)}`
 })
 module.exports = mongoose.model('File', File)
